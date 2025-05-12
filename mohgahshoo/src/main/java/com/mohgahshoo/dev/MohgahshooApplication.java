54bb1dfa407b6e558255645c97c2be1669c46e28
TaeJong <dbxowhdsla12@naver.com>
2025-05-12 16:32:04 +0900

    초기 커밋

---
+++ mohgahshoo/src/main/java/com/mohgahshoo/dev/MohgahshooApplication.java
@@ -0,0 +1,13 @@
+package com.mohgahshoo.dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MohgahshooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MohgahshooApplication.class, args);
+	}
+
+}
---
+++ mohgahshoo/src/test/java/com/mohgahshoo/dev/MohgahshooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mohgahshoo.dev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MohgahshooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
